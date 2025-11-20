9105bfe97752ee446a908e11137e505249266baf
capdaseletni-sys <capdaseletni@gmail.com>
2025-11-20 20:44:07 +0800

    Update kbzmerge.py

--- kbzmerge.py
+++ kbzmerge.py
@@ -12,11 +12,8 @@ playlist_urls = [
 ] 
 
 EPG_URL = "https://github.com/Drewski2423/DrewLive/raw/refs/heads/main/DrewLive.xml.gz"
-OUTPUT_FILE = [ 
-        "all.m3u8",
-        "kbz.m3u8"
-]
-
+OUTPUT_FILE = "all.m3u8"
+        
 def fetch_playlist(url, retries=3, timeout=30):
     headers = {"User-Agent": "Mozilla/5.0"}
     for attempt in range(1, retries + 1):
