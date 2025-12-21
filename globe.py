ea6cf7c6070b335afa10525401239cfb729952a4
capdaseletni-sys <capdaseletni@gmail.com>
2025-12-21 14:48:15 +0800

    Update globe.py

--- globe.py
+++ globe.py
@@ -68,5 +68,5 @@ def process_m3u8(input_file, output_file):
     print(f"Output file: {output_file}")
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     process_m3u8(INPUT_M3U8, OUTPUT_M3U8)
