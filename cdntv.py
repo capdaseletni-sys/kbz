7422474e262b7c2d3193bda1e2d9663451cee8bf
capdaseletni-sys <capdaseletni@gmail.com>
2026-01-04 09:28:07 +0800

    Update cdntv.py

--- cdntv.py
+++ cdntv.py
@@ -4,7 +4,7 @@ import re
 PLAYLIST_URL = "https://raw.githubusercontent.com/doms9/iptv/refs/heads/default/M3U8/events.m3u8"
 OUTPUT_FILE = "cdntv.m3u8"
 
-KEYWORDS = ["(SHARK)", "(CDNTV)","(ROXIE)"]
+KEYWORDS = ["(SHARK)", "(CDNTV)","(ROXIE)","(TVPASS)"]
 
 
 def download_m3u(url: str) -> str:
